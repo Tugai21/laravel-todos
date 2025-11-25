f83f1297b94f2917e588a85e20348a7e6af763b9
Tugai21 <44272337+Tugai21@users.noreply.github.com>
2025-11-25 12:56:54 +0000

    photo upload page

---
+++ laravel-todos/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;    
+use App\Http\Controllers\PhotoController;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+    Route::middleware(['auth'])->group(function () {
+    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::patch('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+});
+Route::middleware(['auth'])->group(function () {
+    Route::get('/photos', [PhotoController::class, 'index'])->name('photos.index');
+    Route::post('/photos', [PhotoController::class, 'store'])->name('photos.store');
+    Route::delete('/photos/{photo}', [PhotoController::class, 'destroy'])->name('photos.destroy');
+});
+
+require __DIR__.'/auth.php';
